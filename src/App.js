6646589320ed4460f94f04fb5c43e3dd4752e7cc
thai.ly <thai.ly@masterisehomes.com>
2020-11-16 11:18:08 +0700

    update config tailwind

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-      <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-        <div className="ml-6 pt-1">
-          <h1 className="text-2xl text-blue-700 leading-tight">
-            Tailwind and Create React App
-          </h1>
-          <p className="text-base text-gray-700 leading-normal text-red-400">
-            Building apps together
-          </p>
-        </div>
+    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg">
+      <div className="ml-6 pt-1">
+        <h1 className="text-2xl text-blue-700 leading-tight">Tailwind and Create React App</h1>
+        <p className="text-base text-gray-700 leading-normal">Building apps together</p>
       </div>
+    </div>
   );
 }
 
